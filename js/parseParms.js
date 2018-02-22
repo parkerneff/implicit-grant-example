9dd2403069408cf00b0b586404d348cb23a752bf
pneff <pneff@unicon.net>
2018-02-22 15:04:40 -0700

    initial

--- js/parseParms.js
+++ js/parseParms.js
@@ -1,4 +1,4 @@
-function getParameterByName(name, url) {
+function zgetParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
@@ -8,8 +8,23 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function loadParm(parmValue) {
-    var parmValue = getParameterByName(parmValue);
+function zloadParm(parmKey) {
+    var parmValue = getParameterByName(parmKey);
     var ul = document.getElementById("parms");
-    ul.appendChild(document.createElement("li").appendChild(document.createTextNode(parmValue)));
+    ul.appendChild(document.createElement("li").appendChild(document.createTextNode(parmKey + ": " + parmValue)));
+}
+function printAccessToken() {
+    var accessToken = getAccessToken();
+    var ul = document.getElementById("parms");
+    ul.appendChild(document.createElement("li").appendChild(document.createTextNode("access_token: " + accessToken)));
+}
+
+
+function getParameterByName(name) {
+    var match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function getAccessToken() {
+    return getParameterByName('access_token');
 }
